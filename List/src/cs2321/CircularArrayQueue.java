26dd8c44f3376cfc51babc971a40826bb05deb66
Samuel Milner <ssjmilner@gmail.com>
2019-09-17 22:18:55 -0400

    Added Comments

--- List/src/cs2321/CircularArrayQueue.java
+++ List/src/cs2321/CircularArrayQueue.java
@@ -16,22 +16,30 @@ public class CircularArrayQueue<E> implements Queue<E> {
 	private int size = 0;
 	private E[] data;
 	private int QueueSize = 16;
-	private int index = 0;
+	private int index = 0; //to keep track of the front of the queue
 
 	public CircularArrayQueue(int queueSize) {
 		data = (E[]) new Object[queueSize];
 	}
-	
+	/**
+	 * @return size of ArrayList
+	 */
 	@Override
 	public int size() {
 		return size;
 	}
-
+	/**
+	 *@return returns boolean value of whether array is empty or not
+	 */
 	@Override
 	public boolean isEmpty() {
 		return size == 0;
 	}
-
+	/**
+	 * Adds new data to end or back of queue and increases size
+	 * @param E e, data to be added
+	 * @throws IndexOutOfBoundsException if queue is already full
+	 */
 	@Override
 	public void enqueue(E e) throws IndexOutOfBoundsException {
 		if (size == QueueSize)
@@ -39,12 +47,18 @@ public class CircularArrayQueue<E> implements Queue<E> {
 	    data[size] = e;
 	    size ++;
 	}
-
+	/**
+	 * @return returns data at front of queue
+	 */
 	@Override
 	public E first() {
 		return data[index];
 	}
-
+	/**
+	 * removes data from front of queue, changes size and moves the index
+	 * @return E temp, data being removed
+	 * @throws IndexOutOfBoundsException if queue is empty
+	 */
 	@Override
 	public E dequeue() throws IndexOutOfBoundsException {
 		E temp = data[index];
--- List/src/cs2321/DoublyLinkedList.java
+++ List/src/cs2321/DoublyLinkedList.java
@@ -50,6 +50,12 @@ public class DoublyLinkedList<E> implements PositionalList<E> {
 		header.setNext(trailer);
 	}
 
+	/**
+	 * Checks to see if Position<E> p is valid, by checking to see if its a instance of the Node being used and checking to see if it is in the list
+	 * @param Position<E> p, position to check
+	 * @return Node<E> node, p recasted as a Node<E>
+	 * @throws IllegalArgumentException if p is invalid or not in lust
+	 */
 	private Node<E> validate(Position<E> p) throws IllegalArgumentException {
 		if (!(p instanceof Node)) throw new IllegalArgumentException("Invalid p");
 		Node<E> node = (Node<E>) p;
@@ -57,45 +63,73 @@ public class DoublyLinkedList<E> implements PositionalList<E> {
 			throw new IllegalArgumentException("p is no longer in the list");
 		return node;
 	}
-
+	/**
+	 *
+	 * @param
+	 * @return
+	 * @throws
+	 */
 	private Position<E> position(Node<E> node) {
 		if(node == header || node == trailer)
 			return null;
 		return node;
 	}
-
+	/**
+	 *
+	 * @return size of List
+	 */
 	@Override
 	public int size() {
 		return size;
 	}
-
+	/**
+	 *@return boolean size
+	 */
 	@Override
 	public boolean isEmpty() {
 		return size == 0;
 	}
-
+	/**
+	 * @return position(header.getNext(), the data which is in the first node after header
+	 */
 	@Override
 	public Position<E> first() {
 		return position(header.getNext());
 	}
-
+	/**
+	 * @return position(trailder.getPrev), the data which is the the previous node to the trailer
+	 */
 	@Override
 	public Position<E> last() {
 		return position(trailer.getPrev());
 	}
-
+	/**
+	 * Check data thats in previous node of node given
+	 * @param Position<E> p, node to check data of previous node
+	 * @return postion(node.getPrev) data that is in node before p
+	 * @throws IllegalArgumentException if p is not valid
+	 */
 	@Override
 	public Position<E> before(Position<E> p) throws IllegalArgumentException {
 		Node<E> node = validate(p);
 		return position(node.getPrev());
 	}
-
+	/**
+	 * Check data thats in next node of node given
+	 * @param Position<E> p, node to check data of next node
+	 * @return postion(node.getNext) data that is in node after p
+	 * @throws IllegalArgumentException if p is not valid
+	 */
 	@Override
 	public Position<E> after(Position<E> p) throws IllegalArgumentException {
 		Node<E> node = validate(p);
 		return position(node.getNext());
 	}
-
+	/**
+	 * Adds new node between two existing nodes, and changes where prev and next node point to
+	 * @param E e, data to add in node & Node<E> prev, node that will be before new node & Node<E> succ, node that will be after new node
+	 * @return Node<E> newest, node that was insterted
+	 */
 	private Position<E> addBetween(E e, Node<E> pred, Node<E> succ) {
 		Node<E> newest = new Node<>(e, pred, succ);
 		pred.setNext(newest);
@@ -103,31 +137,52 @@ public class DoublyLinkedList<E> implements PositionalList<E> {
 		size++;
 		return newest;
 	}
-
+	/**
+	 * adds new node after header and changes where header and previously first node point to
+	 * @param E e, data to add in new node
+	 */
 	@Override
 	public Position<E> addFirst(E e) {
 		return addBetween(e, header, header.getNext());
 	}
-
+	/**
+	 *Adds new node before trailer and cahgnes were previously last node and trailer point to
+	 * @param E e, data to be added to new node
+	 */
 	@Override
 	public Position<E> addLast(E e) {
 		return addBetween(e, trailer.getPrev(), trailer);
 	}
-
+	/**
+	 * Adds new node with data E e before node p thats given
+	 * @param Position<E> p, node to add new node before
+	 * @return addBetween(e, node.getPrev(), node), returns call of addbetween
+	 * @throws IllegalArgumentException if p is not valid
+	 */
 	@Override
 	public Position<E> addBefore(Position<E> p, E e)
 			throws IllegalArgumentException {
 		Node<E> node = validate(p);
 		return addBetween(e, node.getPrev(), node);
 	}
-
+	/**
+	 * Adds new node with data E e after node p thats given
+	 * @param Position<E> p, node to add new node after
+	 * @return addBetween(e, node, node.getNext()), node), returns call of addbetween
+	 * @throws IllegalArgumentException if p is not valid
+	 */
 	@Override
 	public Position<E> addAfter(Position<E> p, E e)
 			throws IllegalArgumentException {
 		Node<E> node = validate(p);
 		return addBetween(e, node, node.getNext());
 	}
-
+	/**
+	 * Sets E e as the new data within a node already in List
+	 * @param Position<E> p, node to put data in, E e, data to add in
+	 * @return E answer, data that was previosuyl in node
+	 * @throws IllegalArgumentException if p is not valid
+	 */
 	@Override
 	public E set(Position<E> p, E e) throws IllegalArgumentException {
 		Node<E> node = validate(p);
@@ -135,7 +190,12 @@ public class DoublyLinkedList<E> implements PositionalList<E> {
 		node.setElement(e);
 		return answer;
 	}
-
+	/**
+	 * Removes p and resents where prev and next ndoes point to, and decreses size of list.
+	 * @param Position<E> p, node to remove
+	 * @return E answer, data that was in removed node
+	 * @throws IllegalArgumentException if node is not valid
+	 */
 	@Override
 	public E remove(Position<E> p) throws IllegalArgumentException {
 		Node<E> node = validate(p);
@@ -151,6 +211,10 @@ public class DoublyLinkedList<E> implements PositionalList<E> {
 		return  answer;
 	}
 
+	/**
+	 * I don't quite understand the Iterator for this yet so I included the comments that the book uses
+	 */
+
 	@Override
 	public Iterator<E> iterator() {
 		return new ElementIterator();
@@ -162,19 +226,21 @@ public class DoublyLinkedList<E> implements PositionalList<E> {
 	}
 
 	private class PositionalIterator implements Iterator<Position<E>> {
+
 		private Position<E> cursor = first();
 		private Position<E> recent = null;
-
+ 		// Tests whether the iterator has a next object.
 		public boolean hasNext() {
 			return (cursor != null);
 		}
+		// Returns the next position in the iterator.
 		public Position<E> next() throws NoSuchElementException {
 			if (cursor == null) throw new NoSuchElementException("nothin left");
 			recent = cursor;
 			cursor = after(cursor);
 			return recent;
 		}
-
+		// Removes the element returned by most recent call to next
 		public void remove() throws IllegalArgumentException {
 			if (recent == null) throw new IllegalArgumentException("nothing to remove");
 			DoublyLinkedList.this.remove(recent);
@@ -183,20 +249,30 @@ public class DoublyLinkedList<E> implements PositionalList<E> {
 	}
 
 	private class PositionalIterable implements Iterable<Position<E>> {
+		//Returns an iterable representation of the list's positions
 		public Iterator<Position<E>> iterator() { return new PositionalIterator(); }
 	}
 
+	//This class adapts the iteration produced by positions() to return elements
 	private class ElementIterator implements Iterator<E> {
 		Iterator<Position<E>> posIterator = new PositionalIterator();
 		public boolean hasNext() {return posIterator.hasNext();}
 		public E next() {return posIterator.next().getElement();}
 		public void remove() { posIterator.remove();}
 	}
-	
+	/**
+	 *removes first node after header
+	 * @return remove(header.next), call of remove
+	 * @throws IllegalArgumentException
+	 */
 	public E removeFirst() throws IllegalArgumentException {
 		return remove(header.next);
 	}
-	
+	/**
+	 *removes node before trailer
+	 * @return remove(trailer.prev), call of remove
+	 * @throws IllegalArgumentException
+	 */
 	public E removeLast() throws IllegalArgumentException {
 		return remove(trailer.prev);
 	}
