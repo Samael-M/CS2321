8e3dce71127c8281d62e85bb7041e409acb5276c
Samuel Milner <ssjmilner@gmail.com>
2019-09-19 20:50:55 -0400

    Started Second Assignment

--- StackQueueApplication/src/cs2321/DLLStack.java
+++ StackQueueApplication/src/cs2321/DLLStack.java
@@ -4,33 +4,34 @@ import net.datastructures.Stack;
 
 public class DLLStack<E> implements Stack<E> {
 
+	DoublyLinkedList<E> Stack = new DoublyLinkedList();
+
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
+		Stack.size();
 		return 0;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
+		Stack.isEmpty();
 		return false;
 	}
 
 	@Override
 	public void push(E e) {
-		// TODO Auto-generated method stub
-		
+		Stack.addFirst(e);
 	}
 
 	@Override
 	public E top() {
-		// TODO Auto-generated method stub
+		Stack.first();
 		return null;
 	}
 
 	@Override
 	public E pop() {
-		// TODO Auto-generated method stub
+		Stack.removeFirst();
 		return null;
 	}
 
--- StackQueueApplication/src/cs2321/Josephus.java
+++ StackQueueApplication/src/cs2321/Josephus.java
@@ -1,5 +1,7 @@
 package cs2321;
 
+import net.datastructures.Position;
+
 public class Josephus {
 	/**
 	 * All persons sit in a circle. When we go around the circle, initially starting
@@ -15,7 +17,26 @@ public class Josephus {
 	 *         the last one in the list is the winner.  
 	 */
 	public DoublyLinkedList<String> order(String[] persons, int k ) {
-		//TODO: implement this method with the help of CircularArrayQueue
-		return null;
-	}	
+		CircularArrayQueue People = new CircularArrayQueue(persons.length);
+		DoublyLinkedList result = new DoublyLinkedList();
+		for(int i = 0; i < persons.length; i++) {
+			People.enqueue(persons[i]);
+		}
+
+		while (result.size() < persons.length) {
+			for (int i = 0; i < k - 1; i++){
+				People.enqueue(People.dequeue());
+			}
+			result.addLast(People.dequeue());
+		}
+		return result;
+	}
+	public static void main(String[] args) {
+		Josephus test = new Josephus();
+		String[] testt = new String[] {"A", "B", "C", "D", "E", "F"};
+		DoublyLinkedList temp = test.order(testt, 3);
+		for (int i = 0; i < 6; i++){
+			System.out.println(temp.removeFirst());
+		}
+	}
 }
