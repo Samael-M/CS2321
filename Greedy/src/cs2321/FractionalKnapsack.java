b292d636ba943196153e4295d8ce341b1027af7b
Samuel Milner <ssjmilner@gmail.com>
2019-10-16 01:05:31 -0400

    Finished assignment 4

--- Greedy/src/cs2321/FractionalKnapsack.java
+++ Greedy/src/cs2321/FractionalKnapsack.java
@@ -17,8 +17,31 @@ public class FractionalKnapsack {
 	 * @return The maximum total benefit. Please use double type operation. For example 5/2 = 2.5
 	 * 		 
 	 */
-	public static double MaximumValue(int[][] items, int knapsackWeight) {
-		 //TODO: Don't forget to modify the return value
-		return 0;
+	public static double MaximumValue(double[][] items, int knapsackWeight) {
+		MaxHeap<Double, double[]> stuff = new MaxHeap<>();
+		for (int i = 0; i <= items.length - 1; i++) {
+			double[] temp = {items[i][0], items[i][1], 0};
+			stuff.insert((double) items[i][1] / items[i][0], temp);
+		}
+		int weight = 0;
+		ArrayList<double[]> taking = new ArrayList<>();
+		int i = 0;
+		while (weight < knapsackWeight) {
+			double temp = stuff.max().getKey();
+			double[] temp2 = stuff.removeMax().getValue();
+			double[] temp3 = {temp, min(temp2[0], knapsackWeight - weight)};
+			taking.add(i, temp3);
+			weight += min(temp2[0], knapsackWeight - weight);
+		}
+		double value = 0;
+		for(double[] s : taking) {
+			value += s[0];
+		}
+		return value;
+	}
+
+	public static double min(double i, double j) {
+		if(i < j) return i;
+		else return  j;
 	}
 }
--- Greedy/src/cs2321/FractionalKnapsackTest.java
+++ Greedy/src/cs2321/FractionalKnapsackTest.java
@@ -7,13 +7,18 @@ import org.junit.Test;
 
 public class FractionalKnapsackTest {
 
+	double[][] sack = {{2, 5}, {3, 6}, {1, 2}, {1, 1}, {1, 4}, {2, 2}, {2, 7}};
+	double[][] sack2 = {};
+	double[][] sack3 = {{3, 6}, {1, 1}};
+	double[][] sack4 = {{1, 3}, {1, 4}, {2, 5}, {3, 7}, {4, 7}, {6, 9}, {7, 8}};
+
 	@Before
 	public void setUp() throws Exception {
 	}
 
 	@Test
 	public void testMaximumValue() {
-		fail("Not yet implemented"); // TODO
+		assertEquals(7, FractionalKnapsack.MaximumValue(sack2, 10));
 	}
 
 }
