aa3e972ddf6e011d166ecffbfee4aa48325a30a0
Samuel <ssjmilner@gmail.com>
2019-11-21 20:23:31 -0600

    finished graph

--- Graph/src/cs2321/AdjListGraph.java
+++ Graph/src/cs2321/AdjListGraph.java
@@ -16,16 +16,48 @@ public class AdjListGraph<V, E> implements Graph<V, E> {
 		private V element;
 		private Position<Vertex<V>> pos;
 		private Map<Vertex<V>, Edge<E>> outgoing, incoming;
-
+		public  InnerVertex(V elem, boolean graphIsDirected) {
+			element = elem;
+			outgoing = new HashMap<>();
+			if(graphIsDirected) incoming = new HashMap<>();
+			else incoming = outgoing;
+		}
 		@Override
 		public V getElement() {
-			return null;
+			return element;
 		}
+		public V setElement(V v) { V old = element; element = v; return  old; }
+		public void setPostion(Position<Vertex<V>> p) {pos = p; }
+		public Position<Vertex<V>> getPosition() { return pos; }
+		public Map<Vertex<V>, Edge<E>> getOuting() { return outgoing; }
+		public Map<Vertex<V>, Edge<E>> getIncoming() { return incoming; }
 	}
 
+	public class InnerEdge<E> implements Edge<E> {
+		private E element;
+		private Position<Edge<E>> pos;
+		private Vertex<V>[] endpoints;
+
+		public InnerEdge(Vertex<V> u, Vertex<V> v, E elem) {
+			element = elem;
+			endpoints = (Vertex<V>[]) new Vertex[]{u, v};
+		}
+		/** Returns the element associated with the edge. */
+		@Override
+		public E getElement() { return element; }
+		public E setElement(E e) { E old = element; element = e; return  old; }
+		public Vertex<V>[] getEndpoints() {return endpoints; }
+		public void setPosition(Position<Edge<E>> p ) { pos = p; }
+		public Position<Edge<E>> getPosition() { return pos; }
+
+	}
+
+	private boolean isDirected;
+	private PositionalList<Vertex<V>> vertices = new DoublyLinkedList<>();
+	private PositionalList<Edge<E>> edges = new DoublyLinkedList<>();
 
 	public AdjListGraph(boolean directed) {
-		//TODO directed graph constructor 
+		isDirected = directed;
 	}
 
 	public AdjListGraph() {
@@ -37,131 +69,178 @@ public class AdjListGraph<V, E> implements Graph<V, E> {
 	 * @see net.datastructures.Graph#edges()
 	 */
 	public Iterable<Edge<E>> edges() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see net.datastructures.Graph#endVertices(net.datastructures.Edge)
-	 */
-	public Vertex[] endVertices(Edge<E> e) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	/* (non-Javadoc)
-	 * @see net.datastructures.Graph#insertEdge(net.datastructures.Vertex, net.datastructures.Vertex, java.lang.Object)
-	 */
-	public Edge<E> insertEdge(Vertex<V> u, Vertex<V> v, E o)
-			throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see net.datastructures.Graph#insertVertex(java.lang.Object)
-	 */
-	public Vertex<V> insertVertex(V o) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see net.datastructures.Graph#numEdges()
-	 */
-	public int numEdges() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	/* (non-Javadoc)
-	 * @see net.datastructures.Graph#numVertices()
-	 */
-	public int numVertices() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	/* (non-Javadoc)
-	 * @see net.datastructures.Graph#opposite(net.datastructures.Vertex, net.datastructures.Edge)
-	 */
-	public Vertex<V> opposite(Vertex<V> v, Edge<E> e)
-			throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/* (non-Javadoc)
-	 * @see net.datastructures.Graph#removeEdge(net.datastructures.Edge)
-	 */
-	public void removeEdge(Edge<E> e) throws IllegalArgumentException {
-
-	}
-
-	/* (non-Javadoc)
-	 * @see net.datastructures.Graph#removeVertex(net.datastructures.Vertex)
-	 */
-	public void removeVertex(Vertex<V> v) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-	
-	}
-
-	/* 
-     * replace the element in edge object, return the old element
-     */
-	public E replace(Edge<E> e, E o) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-    /* 
-     * replace the element in vertex object, return the old element
-     */
-	public V replace(Vertex<V> v, V o) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
+		return edges;
 	}
 
 	/* (non-Javadoc)
 	 * @see net.datastructures.Graph#vertices()
 	 */
 	public Iterable<Vertex<V>> vertices() {
-		// TODO Auto-generated method stub
-		return null;
+		return vertices;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.datastructures.Graph#endVertices(net.datastructures.Edge)
+	 */
+	public Vertex[] endVertices(Edge<E> e) throws IllegalArgumentException {
+		InnerEdge<E> edge = validate(e);
+		return edge.getEndpoints();
+	}
+
+
+	/* (non-Javadoc)
+	 * @see net.datastructures.Graph#insertEdge(net.datastructures.Vertex, net.datastructures.Vertex, java.lang.Object)
+	 */
+	@TimeComplexityExpected("O(1)")
+	public Edge<E> insertEdge(Vertex<V> u, Vertex<V> v, E o)
+			throws IllegalArgumentException {
+		if(getEdge(u, v) == null) {
+			InnerEdge<E> e = new InnerEdge<>(u, v, o);
+			e.setPosition(edges.addLast(e));
+			InnerVertex<V> origin = validate(u);
+			InnerVertex<V> dest = validate(v);
+			origin.getOuting().put(v, e);
+			dest.getIncoming().put(u, e);
+			return e;
+		} else throw  new IllegalArgumentException("Edge from u to v exist");
+	}
+
+	/* (non-Javadoc)
+	 * @see net.datastructures.Graph#insertVertex(java.lang.Object)
+	 */
+	@TimeComplexityExpected("O(1)")
+	public Vertex<V> insertVertex(V o) {
+		InnerVertex<V> v = new InnerVertex<>(o, isDirected);
+		v.setPostion((vertices.addLast(v)));
+		return v;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.datastructures.Graph#numEdges()
+	 */
+	@TimeComplexityExpected("O(1)")
+	public int numEdges() {
+		return edges.size();
+	}
+
+	/* (non-Javadoc)
+	 * @see net.datastructures.Graph#numVertices()
+	 */
+	@TimeComplexityExpected("O(1)")
+	public int numVertices() {
+		return vertices.size();
+	}
+
+	/* (non-Javadoc)
+	 * @see net.datastructures.Graph#opposite(net.datastructures.Vertex, net.datastructures.Edge)
+	 */
+	@TimeComplexityExpected("O(1)")
+	public Vertex<V> opposite(Vertex<V> v, Edge<E> e)
+			throws IllegalArgumentException {
+		InnerEdge<E> edge = validate(e);
+		Vertex<V>[] endpoints = edge.getEndpoints();
+		if(endpoints[0] == v) return endpoints[1];
+		else if(endpoints[1] == v) return endpoints[0];
+		else throw new IllegalArgumentException("V is not incident to this edge");
+	}
+
+	/* (non-Javadoc)
+	 * @see net.datastructures.Graph#removeEdge(net.datastructures.Edge)
+	 */
+	@TimeComplexityExpected("O(1)")
+	public void removeEdge(Edge<E> e) throws IllegalArgumentException {
+		InnerEdge<E> edge = validate(e);
+		InnerVertex<V> v = validate(edge.getEndpoints()[0]);
+		InnerVertex<V> u = validate(edge.getEndpoints()[1]);
+
+		u.getOuting().remove(v);
+		v.getOuting().remove(u);
+
+		edges.remove(edge.getPosition());
+		edge.setPosition(null);
+		edge.setElement(null);
+		edge.endpoints = null;
+	}
+
+	/* (non-Javadoc)
+	 * @see net.datastructures.Graph#removeVertex(net.datastructures.Vertex)
+	 */
+	@TimeComplexityExpected("O(1)")
+	public void removeVertex(Vertex<V> v) throws IllegalArgumentException {
+		InnerVertex<V> vert = validate(v);
+		for (Edge<E> e : vert.getOuting().values())
+			removeEdge(e);
+		for (Edge<E> e : vert.getIncoming().values())
+			removeEdge(e);
+		vertices.remove(vert.getPosition());
+	}
+
+	/* 
+     * replace the element in edge object, return the old element
+     */
+	public E replace(Edge<E> e, E o) throws IllegalArgumentException {
+		InnerEdge<E> edge = validate(e);
+		return edge.setElement(o);
+	}
+
+    /* 
+     * replace the element in vertex object, return the old element
+     */
+	public V replace(Vertex<V> v, V o) throws IllegalArgumentException {
+		InnerVertex<V> edge = validate(v);
+		return edge.setElement(o);
 	}
 
 	@Override
+	@TimeComplexityExpected("O(1)")
 	public int outDegree(Vertex<V> v) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return 0;
+		InnerVertex<V> vert = validate(v);
+		return vert.getOuting().size();
 	}
 
 	@Override
+	@TimeComplexityExpected("O(1)")
 	public int inDegree(Vertex<V> v) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return 0;
+		InnerVertex<V> vert = validate(v);
+		return vert.getIncoming().size();
 	}
 
 	@Override
+	@TimeComplexityExpected("O(n)")
 	public Iterable<Edge<E>> outgoingEdges(Vertex<V> v)
 			throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
+		InnerVertex<V> vert = validate(v);
+		return vert.getOuting().values();
 	}
 
 	@Override
+	@TimeComplexityExpected("O(1)")
 	public Iterable<Edge<E>> incomingEdges(Vertex<V> v)
 			throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
+		InnerVertex<V> vert = validate(v);
+		return vert.getIncoming().values();
 	}
 
 	@Override
+	@TimeComplexityExpected("O(1)")
 	public Edge<E> getEdge(Vertex<V> u, Vertex<V> v)
 			throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return null;
+		InnerVertex<V> origin = validate(v);
+		return origin.getOuting().get(v);
+	}
+
+
+	public InnerVertex<V> validate(Vertex<V> v) {
+		if (!(v instanceof InnerVertex)) throw new IllegalArgumentException("v is not valid vertex");
+		InnerVertex<V> vert = (InnerVertex<V>) v;
+		return (InnerVertex<V>) vert;
+	}
+
+
+	public InnerEdge<E> validate(Edge<E> e) {
+		if(!(e instanceof InnerEdge)) throw new IllegalArgumentException("e is not valid Edge");
+		InnerEdge<E> edge = (InnerEdge<E>) e;
+		return (InnerEdge<E>) edge;
 	}
 	
 }
---
+++ Graph/src/cs2321/DoublyLinkedList.java
@@ -0,0 +1,280 @@
+package cs2321;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+
+import net.datastructures.Position;
+import net.datastructures.PositionalList;
+
+
+public class DoublyLinkedList<E> implements PositionalList<E> {
+
+	private static class Node<E> implements Position<E> {
+		private E element;
+		private Node<E> prev;
+		private Node<E> next;
+		public Node(E e, Node<E> p, Node<E> n) {
+			element = e;
+			prev = p;
+			next = n;
+		}
+		public E getElement() throws IllegalStateException {
+			if (next == null)
+				throw  new IllegalStateException("Position no longert valid");
+			return element;
+		}
+		public Node<E> getPrev() {
+			return prev;
+		}
+		public Node<E> getNext() {
+			return next;
+		}
+		public void setElement(E e) {
+			element = e;
+		}
+		public void setPrev(Node<E> p) {
+			prev = p;
+		}
+		public void setNext(Node<E> n) {
+			next = n;
+		}
+	}
+
+	private Node<E> header;
+	private Node<E> trailer;
+	private int size = 0;
+
+	public DoublyLinkedList() {
+		header = new Node<>(null, null, null);
+		trailer = new Node<>(null, header, null);
+		header.setNext(trailer);
+	}
+
+	/**
+	 * Checks to see if Position<E> p is valid, by checking to see if its a instance of the Node being used and checking to see if it is in the list
+	 * @param Position<E> p, position to check
+	 * @return Node<E> node, p recasted as a Node<E>
+	 * @throws IllegalArgumentException if p is invalid or not in lust
+	 */
+	private Node<E> validate(Position<E> p) throws IllegalArgumentException {
+		if (!(p instanceof Node)) throw new IllegalArgumentException("Invalid p");
+		Node<E> node = (Node<E>) p;
+		if (node.getNext() == null)
+			throw new IllegalArgumentException("p is no longer in the list");
+		return node;
+	}
+	/**
+	 *
+	 * @param
+	 * @return
+	 * @throws
+	 */
+	private Position<E> position(Node<E> node) {
+		if(node == header || node == trailer)
+			return null;
+		return node;
+	}
+	/**
+	 *
+	 * @return size of List
+	 */
+	@Override
+	public int size() {
+		return size;
+	}
+	/**
+	 *@return boolean size
+	 */
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	/**
+	 * @return position(header.getNext(), the data which is in the first node after header
+	 */
+	@Override
+	public Position<E> first() {
+		return position(header.getNext());
+	}
+	/**
+	 * @return position(trailder.getPrev), the data which is the the previous node to the trailer
+	 */
+	@Override
+	public Position<E> last() {
+		return position(trailer.getPrev());
+	}
+	/**
+	 * Check data thats in previous node of node given
+	 * @param Position<E> p, node to check data of previous node
+	 * @return postion(node.getPrev) data that is in node before p
+	 * @throws IllegalArgumentException if p is not valid
+	 */
+	@Override
+	public Position<E> before(Position<E> p) throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		return position(node.getPrev());
+	}
+	/**
+	 * Check data thats in next node of node given
+	 * @param Position<E> p, node to check data of next node
+	 * @return postion(node.getNext) data that is in node after p
+	 * @throws IllegalArgumentException if p is not valid
+	 */
+	@Override
+	public Position<E> after(Position<E> p) throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		return position(node.getNext());
+	}
+	/**
+	 * Adds new node between two existing nodes, and changes where prev and next node point to
+	 * @param E e, data to add in node & Node<E> prev, node that will be before new node & Node<E> succ, node that will be after new node
+	 * @return Node<E> newest, node that was insterted
+	 */
+	private Position<E> addBetween(E e, Node<E> pred, Node<E> succ) {
+		Node<E> newest = new Node<>(e, pred, succ);
+		pred.setNext(newest);
+		succ.setPrev(newest);
+		size++;
+		return newest;
+	}
+	/**
+	 * adds new node after header and changes where header and previously first node point to
+	 * @param E e, data to add in new node
+	 */
+	@Override
+	public Position<E> addFirst(E e) {
+		return addBetween(e, header, header.getNext());
+	}
+	/**
+	 *Adds new node before trailer and cahgnes were previously last node and trailer point to
+	 * @param E e, data to be added to new node
+	 */
+	@Override
+	public Position<E> addLast(E e) {
+		return addBetween(e, trailer.getPrev(), trailer);
+	}
+	/**
+	 * Adds new node with data E e before node p thats given
+	 * @param Position<E> p, node to add new node before
+	 * @return addBetween(e, node.getPrev(), node), returns call of addbetween
+	 * @throws IllegalArgumentException if p is not valid
+	 */
+	@Override
+	public Position<E> addBefore(Position<E> p, E e)
+			throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		return addBetween(e, node.getPrev(), node);
+	}
+	/**
+	 * Adds new node with data E e after node p thats given
+	 * @param Position<E> p, node to add new node after
+	 * @return addBetween(e, node, node.getNext()), node), returns call of addbetween
+	 * @throws IllegalArgumentException if p is not valid
+	 */
+	@Override
+	public Position<E> addAfter(Position<E> p, E e)
+			throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		return addBetween(e, node, node.getNext());
+	}
+	/**
+	 * Sets E e as the new data within a node already in List
+	 * @param Position<E> p, node to put data in, E e, data to add in
+	 * @return E answer, data that was previosuyl in node
+	 * @throws IllegalArgumentException if p is not valid
+	 */
+	@Override
+	public E set(Position<E> p, E e) throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		E answer = node.getElement();
+		node.setElement(e);
+		return answer;
+	}
+	/**
+	 * Removes p and resents where prev and next ndoes point to, and decreses size of list.
+	 * @param Position<E> p, node to remove
+	 * @return E answer, data that was in removed node
+	 * @throws IllegalArgumentException if node is not valid
+	 */
+	@Override
+	public E remove(Position<E> p) throws IllegalArgumentException {
+		Node<E> node = validate(p);
+		Node<E> predecessor = node.getPrev();
+		Node<E> successor = node.getNext();
+		predecessor.setNext(successor);
+		successor.setPrev(predecessor);
+		size--;
+		E answer = node.getElement();
+		node.setElement(null);
+		node.setNext(null);
+		node.setPrev(null);
+		return  answer;
+	}
+
+	/**
+	 * I don't quite understand the Iterator for this yet so I included the comments that the book uses
+	 */
+
+	@Override
+	public Iterator<E> iterator() {
+		return new ElementIterator();
+	}
+
+	@Override
+	public Iterable<Position<E>> positions() {
+		return new PositionalIterable();
+	}
+
+	private class PositionalIterator implements Iterator<Position<E>> {
+
+		private Position<E> cursor = first();
+		private Position<E> recent = null;
+ 		// Tests whether the iterator has a next object.
+		public boolean hasNext() {
+			return (cursor != null);
+		}
+		// Returns the next position in the iterator.
+		public Position<E> next() throws NoSuchElementException {
+			if (cursor == null) throw new NoSuchElementException("nothin left");
+			recent = cursor;
+			cursor = after(cursor);
+			return recent;
+		}
+		// Removes the element returned by most recent call to next
+		public void remove() throws IllegalArgumentException {
+			if (recent == null) throw new IllegalArgumentException("nothing to remove");
+			DoublyLinkedList.this.remove(recent);
+			recent = null;
+		}
+	}
+
+	private class PositionalIterable implements Iterable<Position<E>> {
+		//Returns an iterable representation of the list's positions
+		public Iterator<Position<E>> iterator() { return new PositionalIterator(); }
+	}
+
+	//This class adapts the iteration produced by positions() to return elements
+	private class ElementIterator implements Iterator<E> {
+		Iterator<Position<E>> posIterator = new PositionalIterator();
+		public boolean hasNext() {return posIterator.hasNext();}
+		public E next() {return posIterator.next().getElement();}
+		public void remove() { posIterator.remove();}
+	}
+	/**
+	 *removes first node after header
+	 * @return remove(header.next), call of remove
+	 * @throws IllegalArgumentException
+	 */
+	public E removeFirst() throws IllegalArgumentException {
+		return remove(header.next);
+	}
+	/**
+	 *removes node before trailer
+	 * @return remove(trailer.prev), call of remove
+	 * @throws IllegalArgumentException
+	 */
+	public E removeLast() throws IllegalArgumentException {
+		return remove(trailer.prev);
+	}
+
+}
